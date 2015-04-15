e85d1c2240d88732d0bd97f20e5f82da3d8d8c95
oohusl <oohusl.gmail.com>
2015-04-15 14:11:33 +0800

    WxMpConfigStorage增加partnerId、partnerKey

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/WxMpConfigStorage.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/WxMpConfigStorage.java
@@ -4,67 +4,74 @@ import me.chanjar.weixin.common.bean.WxAccessToken;
 
 /**
  * 微信客户端配置存储
- * @author chanjarster
  *
+ * @author chanjarster
  */
 public interface WxMpConfigStorage {
 
-  public String getAccessToken();
+    public String getAccessToken();
 
-  public boolean isAccessTokenExpired();
+    public boolean isAccessTokenExpired();
 
-  /**
-   * 强制将access token过期掉
-   */
-  public void expireAccessToken();
+    /**
+     * 强制将access token过期掉
+     */
+    public void expireAccessToken();
 
-  /**
-   * 应该是线程安全的
-   * @param accessToken
-   */
-  public void updateAccessToken(WxAccessToken accessToken);
+    /**
+     * 应该是线程安全的
+     *
+     * @param accessToken
+     */
+    public void updateAccessToken(WxAccessToken accessToken);
 
-  /**
-   * 应该是线程安全的
-   * @param accessToken
-   * @param expiresIn
-   */
-  public void updateAccessToken(String accessToken, int expiresIn);
+    /**
+     * 应该是线程安全的
+     *
+     * @param accessToken
+     * @param expiresIn
+     */
+    public void updateAccessToken(String accessToken, int expiresIn);
 
-  public String getJsapiTicket();
+    public String getJsapiTicket();
 
-  public boolean isJsapiTicketExpired();
+    public boolean isJsapiTicketExpired();
 
-  /**
-   * 强制将jsapi ticket过期掉
-   */
-  public void expireJsapiTicket();
+    /**
+     * 强制将jsapi ticket过期掉
+     */
+    public void expireJsapiTicket();
 
-  /**
-   * 应该是线程安全的
-   * @param jsapiTicket
-   */
-  public void updateJsapiTicket(String jsapiTicket, int expiresInSeconds);
+    /**
+     * 应该是线程安全的
+     *
+     * @param jsapiTicket
+     */
+    public void updateJsapiTicket(String jsapiTicket, int expiresInSeconds);
 
-  public String getAppId();
+    public String getAppId();
 
-  public String getSecret();
+    public String getSecret();
 
-  public String getToken();
+    public String getPartnerId();
 
-  public String getAesKey();
+    public String getPartnerKey();
 
-  public long getExpiresTime();
+    public String getToken();
 
-  public String getOauth2redirectUri();
+    public String getAesKey();
 
-  public String getHttp_proxy_host();
+    public long getExpiresTime();
 
-  public int getHttp_proxy_port();
+    public String getOauth2redirectUri();
 
-  public String getHttp_proxy_username();
+    public String getHttp_proxy_host();
+
+    public int getHttp_proxy_port();
+
+    public String getHttp_proxy_username();
 
 
-  public String getHttp_proxy_password();
+    public String getHttp_proxy_password();
 
 }
