ce5311c0ebb69604d52417fa4d61369327303649
dylan <0095103cn@gmail.com>
2017-07-31 17:48:26 +0800

    fix(File): 修复文件关闭
    修复文件关闭

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/util/http/okhttp/OkhttpQrCodeRequestExecutor.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/util/http/okhttp/OkhttpQrCodeRequestExecutor.java
@@ -46,6 +46,7 @@ public class OkhttpQrCodeRequestExecutor extends QrCodeRequestExecutor<OkHttpCli
     try (BufferedSink sink = Okio.buffer(Okio.sink(temp))) {
       sink.writeAll(response.body().source());
     }
+    temp.deleteOnExit();
     return temp;
   }
 }
